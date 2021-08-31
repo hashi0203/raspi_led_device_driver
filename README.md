3843a487e3f3353da571feac863d8ed6537cdb23
hashi0203 <n.hsmt1123@gmail.com>
2021-08-31 03:39:51 +0000

    myled5, add device file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # raspi_led_device_driver
+
+```bash
+$ make
+$ sudo insmod myled.ko
+```
+
+```bash
+$ tail /var/log/syslog
+...
+Aug 31 03:29:46 ubuntu kernel: [1541519.384880] /home/ubuntu/raspi_led_device_driver/myled.c is loaded. major:499
+```
+
+```bash
+$ sudo mknod /dev/myled0 c 499 0
+$ sudo chmod 666 /dev/myled0
+$ echo a > /dev/myled0
+$ tail /var/log/syslog
+...
+Aug 31 03:36:28 ubuntu kernel: [1541921.302421] led_write is called
+Aug 31 03:36:28 ubuntu kernel: [1541921.302437] led_write is called
+```
+
+```bash
+$ sudo rm /dev/myled0
+$ sudo rmmod myled
+```
--- myled.c
+++ myled.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 MODULE_AUTHOR("Nobuho Hashimoto");
 MODULE_DESCRIPTION("driver for LED control");
@@ -7,22 +8,43 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
 static dev_t dev;
+static struct cdev cdv;
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
+{
+    printk(KERN_INFO "led_write is called\n");
+    return 1;
+}
+
+static struct file_operations led_fops = {
+    .owner = THIS_MODULE,
+    .write = led_write
+};
 
 static int __init init_mod(void)
 {
     int retval;
     retval = alloc_chrdev_region(&dev, 0, 1, "myled");
     if (retval < 0) {
-        printk(KERN_ERR "alloc_chrdev_region failure.\n");
+        printk(KERN_ERR "alloc_chrdev_region failed.\n");
         return retval;
     }
 
     printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
+
+    cdev_init(&cdv, &led_fops);
+    cdv.owner = THIS_MODULE;
+    retval = cdev_add(&cdv, dev, 1);
+    if (retval < 0) {
+        printk(KERN_ERR "cdev_add failed. major:%d, minor:%d.\n", MAJOR(dev), MINOR(dev));
+        return retval;
+    }
     return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
+    cdev_del(&cdv);
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
 }
